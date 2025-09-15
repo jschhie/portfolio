607be6b91971857904a83bc4f7c1e92153cc0328
Jennifer Chhie <22698132+jschhie@users.noreply.github.com>
2025-09-15 14:30:40 -0700

    update bio, RWD, prettier formatting

--- website/views.py
+++ website/views.py
@@ -5,17 +5,17 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=["GET"])
 @views.route('/home', methods=["GET"])
 def home():
-    return render_template('home.html')
+  return render_template('home.html')
 
 
 
 @views.route('/about', methods=["GET"])
 def about():
-    return render_template('about.html')
+  return render_template('about.html')
 
 
 
 @views.route('/projects', methods=["GET"])
 def projects():
-    return render_template('projects.html')
+  return render_template('projects.html')
 
